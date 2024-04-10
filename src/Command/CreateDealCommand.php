9b81db8a1cc4889a57f1bcea96664bdddeee450d
daniazulai <dani@d2concepts.nl>
2024-04-10 19:37:29 +0200

    Updated CreateDeal command to add relation with product

--- src/Command/CreateDealCommand.php
+++ src/Command/CreateDealCommand.php
@@ -6,8 +6,10 @@ namespace App\Command;
 
 use GuzzleHttp\Exception\GuzzleException;
 use Pipedrive\Api\DealsApi;
+use Pipedrive\Api\ProductsApi;
 use Pipedrive\ApiException;
 use Pipedrive\Model\NewDeal;
+use Pipedrive\Model\NewDealProduct;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -21,19 +23,43 @@ class CreateDealCommand extends PipedriverCommand
     {
         $this
             ->addArgument('title', InputArgument::REQUIRED, 'Deal title')
+            ->addArgument('product_id', InputArgument::OPTIONAL, 'Product ID')
+            ->addArgument('quantity', InputArgument::OPTIONAL, 'Quantity', 1)
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        /** @var DealsApi $apiInstance */
-        $apiInstance = $this->pipedriveService->getApiInstance('Deals');
-        $deal = new NewDeal([
-            'title' => $input->getArgument('title'),
-        ]);
+        /** @var ProductsApi $productsApi */
+        $productsApi = $this->pipedriveService->getApiInstance('Products');
+        /** @var DealsApi $dealsApi */
+        $dealsApi = $this->pipedriveService->getApiInstance('Deals');
 
         try {
-            $apiInstance->addDeal($deal);
+            $deal = new NewDeal([
+                'title' => $input->getArgument('title'),
+            ]);
+            $deal = $dealsApi->addDeal($deal);
+
+            if ($productId = $input->getArgument('product_id')) {
+                $product = $productsApi->getProduct($productId);
+                if ($product->getSuccess()) {
+                    $price = 0;
+                    foreach ($product->getData()->getPrices() as $currentPrice) {
+                        if ($currentPrice['currency'] === 'EUR') {
+                            $price = (int)$currentPrice['price'];
+                            break;
+                        }
+                    }
+
+                    $dealProduct = new NewDealProduct([
+                        'product_id' => $product->getData()->getId(),
+                        'item_price' => $price,
+                        'quantity' => (int) $input->getArgument('quantity'),
+                    ]);
+                    $dealsApi->addDealProduct($deal->getData()->getId(), $dealProduct);
+                }
+            }
         } catch (GuzzleException $e) {
             return Command::FAILURE;
         } catch (ApiException $e) {
